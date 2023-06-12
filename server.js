cd49587b211f120c0079e220dbdd6950a130968e
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-06-12 12:59:40 -0500

    got menu working and color added

--- server.js
+++ server.js
@@ -17,6 +17,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const path = require('path');
+const chalk = require('chalk');
 
 const connectionSettings = require('./config/connection');
 
@@ -24,9 +25,21 @@ const connectionSettings = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+
+  //Starting the Server
+  app.listen(PORT, () => {
+    console.log(chalk.dim(`App listening at http://localhost:${PORT} 🚀\n`));
+    console.log(chalk.bgMagenta.bold('Employee Tracker Application'));
+    startProgram();
+  });
+
 //Create a function that prompts the user for what action they should take
 function startProgram() {
-  inquirer.prompt ({
+  inquirer.prompt({
     type: 'list',
     name: 'menu',
     message: 'What would you like to do?',
@@ -42,37 +55,36 @@ function startProgram() {
     ]
   })
 
-  .then((answer) => {
-    switch (anwser.menu) {
-      case 'View all departments':
-        viewDepartments();
-        break;
-      case 'View all roles':
-        viewRoles();
-        break;
-      case 'View all employees':
-        viewEmployees();
-        break;
-      case 'Add a department':
-        addDepartment();
-        break;
-      case 'Add a role':
-        addRole();
-        break;
-      case 'Add an employee':
-        addEmployee();
-        break;
-      case 'Update an employee role':
-        updateEmployee();
-        break;
-      case 'Exit';
-        
-    }
-}
-
+    .then((answer) => {
+      switch (anwser.menu) {
+        case 'View all departments':
+          viewDepartments();
+          break;
+        case 'View all roles':
+          viewRoles();
+          break;
+        case 'View all employees':
+          viewEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployee();
+          break;
+        case 'Exit':
+      }
+    })
+  }
 //Function Junction (look to maybe export some functions out of file to keep it clean)
 // function viewDepartments() {
-  
+
 // }
 
 // function viewRoles() {
@@ -99,16 +111,3 @@ function startProgram() {
 
 // }
 
-
-//Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-//Starting the Server
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
-
-//Start the Prompt
-startProgram();
